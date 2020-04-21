16e1b30bb2bab6d37adb41d80ba546131c312ada
White rabbit <34213702+simplenikname@users.noreply.github.com>
2020-04-21 15:45:26 +0300

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,56 @@
+import os
+
+from flask import render_template, request, url_for
+
+from app import app
+from app.client.control import CtrlWrapper
+
+ctrl = CtrlWrapper(CtrlWrapper.load_from_json('./app/client/settings.json'))
+
+
+@app.after_request
+def add_header(response):
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=12'
+    return response
+
+
+@app.route('/')
+@app.route('/control')
+def control():
+    return render_template('control.html')
+
+
+@app.route('/settings', methods=['POST', 'GET'])
+def settings():
+    global ctrl
+    if request.method == 'POST':
+        content = request.get_json()
+        if content['action'] == 'start':
+            if len(ctrl.running_bots) == 0:
+                ctrl = CtrlWrapper(content['config'])
+                ctrl.start()
+            else:
+                return 'Бот уже запущен!'
+        elif content['action'] == 'stop':
+            ctrl.stop_all_bots()
+    return render_template('settings.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return '404'
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path, endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
