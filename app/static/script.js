6a3d19534605e3845f90bed1ec5722e67e987eaf
notfound <48384875+charismaticdemon@users.noreply.github.com>
2020-04-24 12:49:08 +0300

    Исправлены названия настроек

--- app/static/script.js
+++ app/static/script.js
@@ -1,46 +1,46 @@
 let settings = {
-  START_MODE: "console",
+    START_MODE: "console",
 
-  LOG_TO_FILE: false,
-  LOG_TO_CONSOLE: true,
-  LOG_REQUESTS_TO_SERVER: true,
-  LOGGING_LEVEL_DEBUG: false,
+    LOG_TO_FILE: false,
+    LOG_TO_CONSOLE: true,
+    LOG_LEVEL_DEBUG: false,
+    LOG_REQUESTS_TO_SERVER: true,
 
-  MULTIPLE_MODE: false,
-  SIMPLIFIED_ALGORITHMS_MODE: true,
-  INFINITE_MODE: false
+    MULTIPLE_MODE: false,
+    INFINITY_MODE: false,
+    SIMPLIFIED_ALGORITHMS_MODE: true
 };
 
 const DEBUG = true;
 
 function set_listener(identifier, proprty) {
-  try {
-    document.getElementById(identifier).onchange = function () {
-      proprty == true ? (proprty = false) : (proprty = true);
-      console.log(`${identifier} ${proprty}`);
-    };
-  } catch (e) {
-    if (DEBUG) {
-      console.log("Ошибка: " + e);
+    try {
+        document.getElementById(identifier).onchange = function() {
+            proprty == true ? (proprty = false) : (proprty = true);
+            console.log(`${identifier} ${proprty}`);
+        };
+    } catch (e) {
+        if (DEBUG) {
+            console.log("Ошибка: " + e);
+        }
     }
-  }
 }
 
 function postData(url = "", data = {}) {
-  // let t = JSON.stringify(data)
-  try {
-    let send = JSON.stringify(data);
-    console.log(send);
-    fetch(url, {
-      method: "POST",
-      body: send,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error) {
-    console.error("Ошибка:", error);
-  }
+    // let t = JSON.stringify(data)
+    try {
+        let send = JSON.stringify(data);
+        console.log(send);
+        fetch(url, {
+            method: "POST",
+            body: send,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    } catch (error) {
+        console.error("Ошибка:", error);
+    }
 }
 
 //=================================//
@@ -48,31 +48,35 @@ function postData(url = "", data = {}) {
 //=================================//
 
 if (document.title == "Контроль бота") {
-  // control.html
-  set_listener("log_to_file_checkbox", settings.LOG_TO_FILE);
-  set_listener("log_requests_to_server_checkbox", settings.LOG_REQUESTS_TO_SERVER);
-  set_listener("log_to_console_checkbox", settings.LOG_TO_CONSOLE);
-  set_listener("logging_level_debug_checkbox", settings.LOGGING_LEVEL_DEBUG);
-  set_listener("infinite_checkbox", settings.INFINITE_MODE);
+
+    // control.html
+    set_listener("infinity_checkbox", settings.INFINITY_MODE);
+    set_listener("log_to_file_checkbox", settings.LOG_TO_FILE);
+    set_listener("log_to_console_checkbox", settings.LOG_TO_CONSOLE);
+    set_listener("log_level_debug_checkbox", settings.LOG_LEVEL_DEBUG);
+    set_listener("log_requests_to_server_checkbox", settings.LOG_REQUESTS_TO_SERVER);
+
 } else if (document.title == "Настройки") {
-  // settings.html
-  set_listener("multiple_mode_checkbox", settings.MULTIPLE_MODE);
-  set_listener("simplefield_mode_checkbox", settings.SIMPLIFIED_ALGORITHMS_MODE);
+
+    // settings.html
+    set_listener("multiple_mode_checkbox", settings.MULTIPLE_MODE);
+    set_listener("simplified_mode_checkbox", settings.SIMPLIFIED_ALGORITHMS_MODE);
+
 }
 
 //==================================//
 // описание post запросов к серверу //
 //==================================//
 
-document.getElementById("panel__start").onclick = function () {
-  postData(document.baseURI + "settings", {
-    action: "start",
-    config: settings,
-  });
+document.getElementById("panel__start").onclick = function() {
+    postData(document.baseURI + "settings", {
+        action: "start",
+        config: settings,
+    });
 };
-document.getElementById("panel__stop").onclick = function () {
-  postData(document.baseURI + "settings", {
-    action: "stop",
-    config: settings,
-  });
+document.getElementById("panel__stop").onclick = function() {
+    postData(document.baseURI + "settings", {
+        action: "stop",
+        config: settings,
+    });
 };
